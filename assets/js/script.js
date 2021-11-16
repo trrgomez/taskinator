e57bf611fb668b1598eef58c1cb5cf83755b4f09
trgomez <88842062+trrgomez@users.noreply.github.com>
2021-11-16 13:27:47 -0600

    updated to get items from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -214,6 +214,51 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function() {
+  tasks = localStorage.getItem("tasks")
+  console.log(tasks)
+
+  if(tasks === null){
+    tasks = []
+    return false
+  }
+
+  tasks = JSON.parse(tasks);
+  console.log(tasks)
+
+  for(var i =0; i < tasks.length; i++){
+    tasks[i].id = taskIdCounter;
+    console.log(tasks[i])
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.setAttribute('data-task-id', tasks[i].id)
+    console.log(listItemEl)
+    var taskInfoEl = document.createElement('div')
+    taskInfoEl.className = "task-info"
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+    listItemEl.appendChild(taskInfoEl)
+    var taskActionsEl = createTaskActions(tasks[i].id)
+    listItemEl.appendChild(taskActionsEl)
+    console.log(listItemEl);
+    if(tasks[i].status === 'to do'){
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 0
+      listItemEl.appendChild(tasksToDoEl)
+    }else if(tasks[i].status === 'in progress'){
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 1
+      listItemEl.appendChild(tasksInProgressEl)
+    }else if(tasks[i].status === 'complete'){
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 2
+      listItemEl.appendChild(tasksCompletedEl)
+    }
+
+    taskIdCounter++
+    console.log(listItemEl)
+  }
+}
+
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+
+loadTasks()
